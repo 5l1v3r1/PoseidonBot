aee0a6786b975b6a5e5293935ceebaf23e764055
Juan Alvarez <Juan Alvarez>
2016-12-21 21:56:43 -0400

    removed config.json and am now using config variables from heroku

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 'use strict';
 
 const TelegramBot = require('node-telegram-bot-api');
-const fs = require('fs');
 
-const CONFIG_FILE_NAME = 'config.json';
-
-let config = JSON.parse(fs.readFileSync(CONFIG_FILE_NAME));
-
-let token = config.token || process.env.TOKEN;
+let token = process.env.TOKEN;
+console.log(token);
 
 let bot = new TelegramBot(token, {
   polling: true
@@ -21,6 +17,8 @@ for (let x = 0; x < 1000; x++)
 bot.on('message', (msg) => {
   let chatId = msg.chat.id;
   let msgText = msg.text;
+  if (!msgText)
+    return;
   let split = msg.text.split(' ');
   if (split[0] === '/flood')
     bot.sendMessage(chatId, flood);
