d4abf8d0e0518e80e84dcd6b716beca1faa7df6c
Juan Alvarez <Juan Alvarez>
2016-12-21 22:20:35 -0400

    Added alternative command

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 'use strict';
 
 const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
 
-let token = process.env.TOKEN;
+const CONFIG_FILE_NAME = 'config.json';
+const config = JSON.parse(fs.readFileSync(CONFIG_FILE_NAME));
+
+const WAVE = '🌊';
+const ROW_LENGTH = 22;
+
+let token = config.token;
 
 let bot = new TelegramBot(token, {
   polling: true
 });
 
-const wave = '🌊';
 let flood = '';
-for (let x = 0; x < 1000; x++)
-  flood += wave;
+for (let x = 0; x < ROW_LENGTH * 50; x++)
+  flood += WAVE;
 
 bot.on('message', (msg) => {
   let chatId = msg.chat.id;
@@ -19,6 +25,7 @@ bot.on('message', (msg) => {
   if (!msgText)
     return;
   let split = msg.text.split(' ');
-  if (split[0] === '/flood')
+  let command = split[0];
+  if (command === '/flood' || command === '/flood@PoseidonFloodBot')
     bot.sendMessage(chatId, flood);
 });
